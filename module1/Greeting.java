f1f0d7c27884cddfb4528a189b4bd7885c84bd8c
D. Cristina Terry <crisdc88@gmail.com>
2026-08-23 17:01:24 -0500

    Add Module 1 activity: Hello World and Greeting programs

---
+++ module1/Greeting.java
@@ -0,0 +1,14 @@
+
+public class Greeting {
+    public static void main(String[] args) { 
+        // Scanner scanner = new Scanner(System.in); 
+        // System.out.print("What is your name? "); 
+        // String name = scanner.nextLine(); 
+        // System.out.println("Hello, " + name + "! Welcome to the program."); 
+        //System.out.println("What do you do for work");
+        //String work = scanner.nextLine();
+        //System.out.println("Hello ,"+ name + "! As a "+ work +", you are in exactly the right place.");
+        //scanner.close(); 
+
+    } 
+}
---
+++ module1/HelloWorld.java
@@ -0,0 +1,8 @@
+/**
+ * @param args
+ */
+public class HelloWorld { 
+    public static void main(String[] args) { 
+        System.out.println("Hello, world! second try"); 
+    } 
+}
